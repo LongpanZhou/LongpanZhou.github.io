ca788dd7ee850b0171acbf36fab78069e1bc9f25
LongpanZhou <zhoul83@mcmaster.ca>
2024-10-15 22:17:01 -0400

    Trying to fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Clicks from './animalclicks/clicks.tsx';
 import './metaball.js';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/animalclicks" element={<Clicks />} />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/animalclicks" element={<Clicks/>} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
 );
